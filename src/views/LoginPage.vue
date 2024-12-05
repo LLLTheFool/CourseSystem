256498b33525b782ecda8b3f7e81c275e06db974
zjt2021150118 <1507883427@qq,com>
2024-12-05 20:44:08 +0800

    12月5日提交，完成了首页、学生管理功能

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -50,7 +50,7 @@ export default{
         <div class="Box">
             <h1>登录</h1>
             <div class="InputBox">
-                <input type="text" v-model.trim="name" @keyup.enter="Login" placeholder="请输入账号">
+                <input s type="text" v-model.trim="name" @keyup.enter="Login" placeholder="请输入账号">
             </div>
             <div class="line1"></div>
 
@@ -69,91 +69,98 @@ export default{
         </div>
     </div>
 </template>
-
 <style scoped>
-.container{
+.container {
     height: 100vh;
-
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.Box{
+
+.Box {
     width: 480px;
     height: 370px;
-
-    border:1px solid rgba(0,0,0,0.2);
+    border: 1px solid rgba(0, 0, 0, 0.2);
     border-radius: 15px;
+    padding: 20px; /* 添加适当的内边距 */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
-.Box h1{
+
+.Box h1 {
     color: black;
     text-align: center;
     font-size: 40px;
     font-family: "StSong";
 }
-.Box input{
-    width: 300px;
-    height: 30px;
-    border: none;
-    outline: none;
 
-    font-size: 14px;
-}
-.InputBox{
-    width: 350px;
-    height: 25px;
-    padding: 10px;
+.InputBox {
+    width: 100%;  /* 使容器宽度充满 */
+    height: 50px; /* 设置合适的高度 */
+    padding: 0;
     margin-bottom: 8px;
     margin-left: auto;
     margin-right: auto;
-    border:1px solid rgba(0,0,0,0.2);
-    border-radius: 15px;
-
     display: flex;
     align-items: center;
     justify-content: center;
-
-    border:1px solid rgba(0,0,0,0.2);
-    border-radius: 15px;
 }
 
-.logo{
+.Box input {
+    width: 100%;  /* 让输入框填满父容器 */
+    height: 30px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    outline: none;
+    font-size: 14px;
+    border-radius: 15px;
+    padding-left: 10px; /* 给输入框添加内边距 */
+    background-color: white; /* 设置背景色为白色 */
+}
+
+.logo {
     width: 28px;
     height: 28px;
     margin-right: 10px;
 }
-.btnBox{
+
+.btnBox {
     margin-top: 30px;
+    display: flex;  /* 设置为 Flex 容器 */
+    justify-content: center;  /* 水平居中 */
+    align-items: center;  /* 垂直居中 */
 }
-.loginBtn{
+
+.loginBtn {
     width: 260px;
     height: 45px;
-    margin-left: 110px;
     border-radius: 15px;
-
-    background-color: rgba(80,0,0);
+    background-color: rgba(80, 0, 0);
     color: white;
     font-size: 25px;
     font-weight: bold;
 }
-.tip{
+
+
+.tip {
     color: red;
     font-weight: bolder;
     font-size: 15px;
     text-align: center;
 }
-.line1{
-    width: 400px;
+
+.line1 {
+    width: 100%; /* 让线条宽度自适应 */
     margin-left: 40px;
     margin-bottom: 30px;
-    
-    border-bottom: 1px solid rgba(0,0,0,0.2);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
 }
-.line2{
-    width: 400px;
+
+.line2 {
+    width: 100%; /* 让线条宽度自适应 */
     margin-left: 40px;
     margin-bottom: 10px;
-
-    border-bottom: 1px solid rgba(0,0,0,0.2);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
 }
 </style>
